62a96b6f22d02418fb3916fa2491ee8d99fb4ae8
Aoichaan0513 <aoichaan0513@gmail.com>
2022-06-08 21:43:28 +0900

    [Update] index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,10 @@
+import { CSSProperties, ReactNode } from 'react';
+
+export interface BaseProps {
+    children?: ReactNode;
+    className?: string;
+    style?: CSSProperties;
+}
+
 export * from './Menu';
 export * from './Select';
